72d866b742232065aa48682c2d0fa182675a529c
Wrlan Silva <157828195+wrlandev@users.noreply.github.com>
2024-01-26 18:46:28 -0300

    Manipulando Variáveis

---
+++ Dev_Dotnet/ManipulandoVariaveis/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml;
+
+string apresentacao = "Olá seja bem vindo!";
+Console.WriteLine(apresentacao);
+
+int credito = 1;
+Console.WriteLine($"Você tem {credito} crédito disponível.");
+
+double altura = 1.80;
+Console.WriteLine($"Você tem {altura} de altura.");
+
+decimal preco = 1.00M;
+Console.WriteLine($"O preço e de ${preco}.");
+
+bool condicao = true;
+Console.WriteLine($"Sua condição e {condicao}.");
