f9e62159d5f5d191d5d408fb9a020e38021df52c
Wrlan Silva <157828195+wrlandev@users.noreply.github.com>
2024-01-26 19:18:26 -0300

    Ordem de operadores

---
+++ Dev_Dotnet/OrdemDosOperadores/Program.cs
@@ -0,0 +1,11 @@
+﻿double a = 4 / 2 + 2;
+Console.WriteLine(a);
+
+double b = 4 / (2 + 2);
+Console.WriteLine(b);
+
+double y = 4 * 2 + 2;
+Console.WriteLine(y);
+
+double z = 4 * (2 + 2);
+Console.WriteLine(z);
