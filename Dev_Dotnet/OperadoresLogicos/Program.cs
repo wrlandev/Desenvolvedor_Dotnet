66f456ebcf2a29bbd8587ae927b775caf7c33d9e
Wrlan Silva <157828195+wrlandev@users.noreply.github.com>
2024-01-26 20:01:55 -0300

    Operadores lógicos

---
+++ Dev_Dotnet/OperadoresLogicos/Program.cs
@@ -0,0 +1,35 @@
+﻿bool maiorDeIdade = false;
+bool autorizado  = false;
+
+if (maiorDeIdade || autorizado)
+{
+    Console.WriteLine("Entrada liberada!");
+}
+else
+{
+    Console.WriteLine("Entrada não liberada!");
+}
+
+bool presencaMinima = true;
+double media = 6.5;
+
+if (presencaMinima && media >= 7)
+{
+    Console.WriteLine("Aprovado!");
+}
+else
+{
+    Console.WriteLine("Reprovado!");
+}
+
+bool choveu = false;
+bool estaTarde = false;
+
+if(!choveu && !estaTarde)
+{
+    Console.WriteLine("Vou pedalar!");
+}
+else
+{
+    Console.WriteLine("Não vou pedalar!");
+}
