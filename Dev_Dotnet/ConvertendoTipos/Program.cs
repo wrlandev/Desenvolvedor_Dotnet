6aa30d122340c81c09e1a996f52e83e1e3cb6102
Wrlan Silva <157828195+wrlandev@users.noreply.github.com>
2024-01-26 19:15:06 -0300

    Convertendo tipos

---
+++ Dev_Dotnet/ConvertendoTipos/Program.cs
@@ -0,0 +1,17 @@
+﻿int a = Convert.ToInt32("10");
+int b = int.Parse("10");
+int c = Convert.ToInt32(null);
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+
+int inteiro = 5;
+string x = inteiro.ToString();
+
+Console.WriteLine(x);
+
+int y = 6;
+double z = y;
+
+Console.WriteLine(z);
