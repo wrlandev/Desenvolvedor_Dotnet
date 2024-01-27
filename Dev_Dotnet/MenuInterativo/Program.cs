4550cb1586a64f53d38be4984905701c8b4480d8
Wrlan Silva <157828195+wrlandev@users.noreply.github.com>
2024-01-27 07:29:10 -0300

    Menu

---
+++ Dev_Dotnet/MenuInterativo/Program.cs
@@ -0,0 +1,31 @@
+﻿string opcao;
+
+while(true)
+{
+    Console.Clear();
+    Console.WriteLine("Digite a sua opção");
+    Console.WriteLine("1 - Cadastrar");
+    Console.WriteLine("2 - Buscar");
+    Console.WriteLine("3 - Apagar");
+    Console.WriteLine("4 - Encerrar");
+
+    opcao = Console.ReadLine();
+
+    switch(opcao)
+    {
+        case "1": Console.WriteLine("Cadastrar");
+        break;
+        case "2": Console.WriteLine("Buscar");
+        break;
+        case "3":
+        Console.WriteLine("Apagar");
+        break;
+        case "4":
+        Console.WriteLine("Encerrar");
+        Environment.Exit(0);
+        break;
+
+        default: Console.WriteLine("Opção inválida");
+        break;
+    }
+}
