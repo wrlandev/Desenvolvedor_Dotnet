f45d5eeaca5b243726398c0f48a9aac932fdde90
Wrlan Silva <157828195+wrlandev@users.noreply.github.com>
2024-01-27 09:16:09 -0300

    Array de inteiros

---
+++ Dev_Dotnet/ArrayDeInteiros/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] arrayInteiros = new int[3];
+
+arrayInteiros[0] = 10;
+arrayInteiros[1] = 20;
+arrayInteiros[2] = 30;
+
+Array.Resize(ref arrayInteiros, arrayInteiros.Length * 2);
+
+for (int i = 0; i < arrayInteiros.Length; i++)
+{
+    Console.WriteLine($"Posição n: {i} - {arrayInteiros[i]}");
+}
+
+//
+
+foreach(int valor in arrayInteiros)
+{
+    Console.WriteLine(valor);
+}
