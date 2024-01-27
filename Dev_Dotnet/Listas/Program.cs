dd135a81adb1992b982247e4889f9ebc941cf9eb
Wrlan Silva <157828195+wrlandev@users.noreply.github.com>
2024-01-27 09:22:11 -0300

    Lista

---
+++ Dev_Dotnet/Listas/Program.cs
@@ -0,0 +1,17 @@
+﻿List<string> lista = new List<string>();
+
+lista.Add("SP");
+lista.Add("MG");
+lista.Add("RJ");
+
+for(int i = 0; i < lista.Count; i++)
+{
+    Console.WriteLine($"Posição n: {i} - {lista[i]}");
+}
+
+//
+
+foreach(string str in lista)
+{
+    Console.WriteLine(str);
+}
