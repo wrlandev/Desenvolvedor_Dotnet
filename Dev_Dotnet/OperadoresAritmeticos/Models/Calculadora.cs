040b405e121dea50c8dea5e7b424e7e9bc23d8b1
Wrlan Silva <157828195+wrlandev@users.noreply.github.com>
2024-01-27 06:43:31 -0300

    Operadores Aritméticos

---
+++ Dev_Dotnet/OperadoresAritmeticos/Models/Calculadora.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperadoresAritmeticos.Models
+{
+    internal class Calculadora
+    {
+        public void Somar(int x, int y)
+        {
+            Console.WriteLine($"{x} + {y} = {x + y}");
+        }
+        public void Subtrair(int x, int y)
+        {
+            Console.WriteLine($"{x} - {y} = {x - y}");
+        }
+        public void Multiplicar(int x, int y)
+        {
+            Console.WriteLine($"{x} x {y} = {x * y}");
+        }
+        public void Dividir(int x, int y)
+        {
+            Console.WriteLine($"{x} / {y} = {x / y}");
+        }
+        public void Potencia(int x, int y)
+        {
+            double pot = Math.Pow(x, y);
+            Console.WriteLine($"{x}^{y} = {pot}");
+        }
+        public void Seno(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double seno = Math.Sin(radiano);
+            Console.WriteLine($"Seno de {angulo}° = {seno}");
+        }
+        public void Coseno(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double coseno = Math.Cos(radiano);
+            Console.WriteLine($"Seno de {angulo}° = {coseno}");
+        }
+        public void Tangente(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double tangente = Math.Tan(radiano);
+            Console.WriteLine($"Seno de {angulo}° = {tangente}");
+        }
+        public void RaizQuadrada(double x)
+        {
+            double raiz = Math.Sqrt(x);
+            Console.WriteLine($"Raiz quadrada de {x} = {raiz}");
+        }
+    }
+}
---
+++ Dev_Dotnet/OperadoresAritmeticos/Program.cs
@@ -0,0 +1,15 @@
+﻿using OperadoresAritmeticos.Models;
+
+Calculadora calc = new Calculadora();
+
+calc.Somar(2, 1);
+calc.Subtrair(3, 2);
+calc.Multiplicar(4, 3);
+calc.Dividir(4, 2);
+calc.Potencia(3, 3);
+//
+calc.Seno(30);
+calc.Coseno(30);
+calc.Tangente(30);
+//
+calc.RaizQuadrada(25);
