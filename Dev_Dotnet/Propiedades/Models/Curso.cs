b519a924632a47fb27b23e36cd1b22a50aa50b3d
Wrlan Silva <157828195+wrlandev@users.noreply.github.com>
2024-01-29 18:34:06 -0300

    Propiedades

---
+++ Dev_Dotnet/Propiedades/Models/Curso.cs
@@ -0,0 +1,30 @@
+﻿namespace Propiedades.Models
+{
+    internal class Curso
+    {
+        public string Nome { get; set; }
+        public List<Pessoa> Alunos { get; set; }
+
+        public void ListarAluno()
+        {
+           foreach(Pessoa aluno in  Alunos)
+            {
+                Console.WriteLine(aluno.NomeCompleto);
+            }
+
+        }
+        public int ObterQuantidadeDeAlunos()
+        {
+            int quantidade = Alunos.Count;
+            return quantidade;
+        }
+        public void AdicionarAluno(Pessoa aluno)
+        {
+            Alunos.Add(aluno);
+        }
+        public void RemoverAluno(Pessoa aluno)
+        {
+            Alunos.Remove(aluno);
+        }
+    }
+}
---
+++ Dev_Dotnet/Propiedades/Program.cs
@@ -0,0 +1,12 @@
+﻿using Propiedades.Models;
+
+Pessoa p1 = new("Wrlan", "Silva");
+
+Pessoa p2 = new("João", "Souza");
+
+Curso cursoDeIngles = new();
+cursoDeIngles.Nome = "Ingles";
+cursoDeIngles.Alunos = new List<Pessoa>();
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAluno();
