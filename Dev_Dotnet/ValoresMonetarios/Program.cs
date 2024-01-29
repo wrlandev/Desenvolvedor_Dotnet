401303e97432fd119696c9a4d303de73f0ab6a75
Wrlan Silva <157828195+wrlandev@users.noreply.github.com>
2024-01-29 19:07:43 -0300

    Valores monetários e cultura

---
+++ Dev_Dotnet/ValoresMonetarios/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Globalization;
+
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
+
+decimal valorMonetario = 82356.40M;
+
+Console.WriteLine($"{valorMonetario:C}");
+Console.WriteLine(valorMonetario.ToString("C", CultureInfo.CreateSpecificCulture("pt-BR")));
