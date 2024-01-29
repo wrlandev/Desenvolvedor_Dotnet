3bd1d07e4a74b38779b2fb02471ff8c058d1b6e2
Wrlan Silva <157828195+wrlandev@users.noreply.github.com>
2024-01-29 19:24:22 -0300

    DateTime

---
+++ Dev_Dotnet/DateTime/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+DateTime data = DateTime.Now;
+DateTime data2 = DateTime.Now;
+
+string dataString = "2022-12-17 18:00";
+
+bool sucesso = DateTime.TryParseExact(dataString,
+"yyyy-MM-dd HH:mm",
+CultureInfo.InvariantCulture,
+DateTimeStyles.None, out data2);
+
+Console.WriteLine(data.ToString("dd/MM/yyyy HH:mm"));
+Console.WriteLine(data.ToShortDateString());
+Console.WriteLine(data.ToShortTimeString());
+
+if(sucesso)
+{
+    Console.WriteLine($"Conversão com sucesso {data2}");
+}
