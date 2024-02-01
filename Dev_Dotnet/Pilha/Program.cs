eecedb3efdb09712d2097940ca24fe93bd62f826
Wrlan Silva <157828195+wrlandev@users.noreply.github.com>
2024-02-01 18:53:15 -0300

    Pilha

---
+++ Dev_Dotnet/Pilha/Program.cs
@@ -0,0 +1,15 @@
+﻿Stack<int> pilha = new();
+
+pilha.Push(0);
+pilha.Push(1);
+pilha.Push(2);
+pilha.Push(3);
+pilha.Push(4);
+pilha.Push(5);
+
+pilha.Pop();
+
+foreach (int i in pilha)
+{
+    Console.WriteLine(i);
+}
