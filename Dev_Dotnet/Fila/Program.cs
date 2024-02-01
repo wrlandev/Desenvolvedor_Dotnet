28bb0539aa36f9b206e8b74733eee79899c1e9b9
Wrlan Silva <157828195+wrlandev@users.noreply.github.com>
2024-02-01 18:45:27 -0300

    Fila

---
+++ Dev_Dotnet/Fila/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+
+Queue<int> fila = new();
+
+fila.Enqueue(1);
+fila.Enqueue(2);
+fila.Enqueue(3);
+fila.Enqueue(4);
+fila.Enqueue(5);
+
+fila.Dequeue();
+
+foreach(int i in fila)
+{
+    Console.WriteLine(i);
+}
