10c3ea145d1fcf9362a48298add313699b0e8337
Wrlan Silva <157828195+wrlandev@users.noreply.github.com>
2024-02-01 18:26:47 -0300

    Leitura e Exceção

---
+++ Dev_Dotnet/LeituraDeArquivo/Program.cs
@@ -0,0 +1,17 @@
+﻿try
+{
+    string[] linahs = File.ReadAllLines("Arquivos\\ArquivoDeLeitura.txt");
+
+    foreach (string linha in linahs)
+    {
+        Console.WriteLine(linha);
+    }
+}
+catch(Exception ex)
+{
+    Console.WriteLine($"Erro genérico {ex.Message}");
+}
+finally
+{
+    Console.WriteLine("Sucesso");
+}
