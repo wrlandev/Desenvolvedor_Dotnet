84f7c6893190f0298b66adf3f005cbf9a5a25d0c
Wrlan Silva <157828195+wrlandev@users.noreply.github.com>
2024-02-01 19:04:50 -0300

    Usando dictionary

---
+++ Dev_Dotnet/UsandoDictionary/Program.cs
@@ -0,0 +1,12 @@
+﻿Dictionary<string, string> estados = new();
+
+estados.Add("SP", "São Paulo");
+estados.Add("RJ", "Rio Janeiro");
+estados.Add("MG", "Minas Gerais");
+estados.Add("BA", "Bahia");
+
+foreach (KeyValuePair<string, string> item in estados)
+{
+    Console.WriteLine($"Chave: {item.Key}, Valor: {item.Value}");
+}
+
