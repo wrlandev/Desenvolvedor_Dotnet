8a20f42a584d31ec4479f0c805261dabd433a6e6
Wrlan Silva <157828195+wrlandev@users.noreply.github.com>
2024-02-07 16:34:29 -0300

    Serialização

---
+++ Dev_Dotnet/Serializacao/Models/Venda.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serializacao.Models
+{
+    internal class Venda
+    {
+        public Venda(int id, string produto, decimal preco, DateTime dataVenda)
+        {
+            Id = id;
+            Produto = produto;
+            Preco = preco;
+            DataVenda = dataVenda;
+        }
+
+        public int Id { get; set; }
+        public string Produto { get; set; }
+        public decimal Preco { get; set; }
+        public DateTime DataVenda { get; set; }
+
+    }
+}
---
+++ Dev_Dotnet/Serializacao/Program.cs
@@ -0,0 +1,21 @@
+﻿using Serializacao.Models;
+using Newtonsoft.Json;
+
+DateTime dataAtual = DateTime.Now;
+List<Venda> listaVendas = new();
+
+Venda d1 = new(1, "Material", 25.00M , dataAtual);
+Venda d2 = new(1, "Software", 2331.00M, dataAtual);
+Venda d3 = new(1, "Hardware", 254.00M, dataAtual);
+Venda d4 = new(1, "PC", 252.00M, dataAtual);
+
+listaVendas.Add(d1);
+listaVendas.Add(d2);
+listaVendas.Add(d3);
+listaVendas.Add(d4);
+
+string serializado = JsonConvert.SerializeObject(listaVendas, Formatting.Indented);
+
+File.WriteAllText("C:\\Dev\\Dotnet\\Formacoes\\Desenvolvedor_Dotnet\\Dev_Dotnet\\Serializacao\\Vendas\\vendas.json", serializado);
+
+Console.WriteLine(serializado);
