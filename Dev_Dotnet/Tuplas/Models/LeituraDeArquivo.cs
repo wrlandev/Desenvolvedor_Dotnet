8ca6ee3cb3eb60bf9bf4243e3c1d84148920d9d0
Wrlan Silva <157828195+wrlandev@users.noreply.github.com>
2024-02-07 14:49:28 -0300

    Tuplas

---
+++ Dev_Dotnet/Tuplas/Models/LeituraDeArquivo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tuplas.Models
+{
+    internal class LeituraDeArquivo
+    {
+        public (bool Sucesso, string[] Linhas, int QuantidadeDeLinhas) LerArquivo(string caminho)
+        {
+            try
+            {
+                string[] linhas = File.ReadAllLines(caminho);
+
+                return (true, linhas, linhas.Count());
+            }
+            catch(Exception ex)
+            {
+                return (false, new string[0], 0);
+            }
+
+        }
+    }
+}
---
+++ Dev_Dotnet/Tuplas/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.Design;
+using Tuplas.Models;
+
+(int Id, string Nome, string Sobrenome) tupla = (1, "Wrlan", "Silva");
+Console.WriteLine(tupla);
+
+LeituraDeArquivo arquivo = new();
+var (sucesso, linhas, quantidadeDeLinhas) = arquivo.LerArquivo(
+    "C:\\" +
+    "Dev\\" +
+    "Dotnet\\" +
+    "Formacoes\\" +
+    "Desenvolvedor_Dotnet\\" +
+    "Dev_Dotnet\\" +
+    "LeituraDeArquivo\\" +
+    "Arquivos\\" +
+    "ArquivoDeLeitura.txt");
+
+if(sucesso)
+{
+    Console.WriteLine("Quantidade de linhas: " + quantidadeDeLinhas);
+    foreach (string linha in linhas)
+    {
+        Console.WriteLine(linha);
+    }
+    
+}
+else
+{
+    Console.WriteLine("Erro");
+}
