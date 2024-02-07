8ef44d890496a4189799b2f02b3b816e2953985a
Wrlan Silva <157828195+wrlandev@users.noreply.github.com>
2024-02-07 17:04:43 -0300

    Deserialização

---
+++ Dev_Dotnet/Deserializacao/Models/Venda.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deserializacao.Models
+{
+    internal class Venda
+    {
+        public int Id { get; set; }
+        public string Produto { get; set; }
+        public decimal Preco { get; set; }
+        public DateTime DataVenda { get; set; }
+    }
+}
+
---
+++ Dev_Dotnet/Deserializacao/Program.cs
@@ -0,0 +1,11 @@
+﻿using Deserializacao.Models;
+using Newtonsoft.Json;
+
+string conteudoArquivo = File.ReadAllText("C:\\Dev\\Dotnet\\Formacoes\\Desenvolvedor_Dotnet\\Dev_Dotnet\\Serializacao\\Vendas\\vendas.json");
+
+List<Venda> listaVenda = JsonConvert.DeserializeObject<List<Venda>>(conteudoArquivo);
+
+foreach(Venda venda in listaVenda)
+{
+    Console.WriteLine($"Id: {venda.Id} Produto: {venda.Produto} Preço: {venda.Preco}");
+}
