7ba8e522349ef72322dc1356205747df220eb4ad
Wrlan Silva <157828195+wrlandev@users.noreply.github.com>
2024-02-07 18:36:41 -0300

    Tipo dinâmico

---
+++ Dev_Dotnet/TipoDinamico/Program.cs
@@ -0,0 +1,11 @@
+﻿dynamic variavelDinamica = 4;
+
+Console.WriteLine($"{variavelDinamica.GetType()}");
+
+variavelDinamica = "texto";
+
+Console.WriteLine($"{variavelDinamica.GetType()}");
+
+variavelDinamica = true;
+
+Console.WriteLine($"{variavelDinamica.GetType()}");
