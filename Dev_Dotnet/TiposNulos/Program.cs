41cee24e339a5e3d3948f3f1d6f7488733d0adeb
Wrlan Silva <157828195+wrlandev@users.noreply.github.com>
2024-02-07 18:06:16 -0300

    Tipo nulo

---
+++ Dev_Dotnet/TiposNulos/Program.cs
@@ -0,0 +1,10 @@
+﻿bool? receberEmail = null;
+
+if (receberEmail.HasValue && receberEmail.Value)
+{
+    Console.WriteLine("O usuário quer receber email?");
+}
+else
+{
+    Console.WriteLine("O usuário não respondeu ou não quer receber email");
+}
