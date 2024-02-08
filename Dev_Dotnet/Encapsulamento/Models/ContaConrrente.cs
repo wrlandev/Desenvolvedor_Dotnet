7a4eec7622eb3f1b208f16f9a94d3d1c51cb764f
Wrlan Silva <157828195+wrlandev@users.noreply.github.com>
2024-02-08 20:41:03 -0300

    Encapsulamento

---
+++ Dev_Dotnet/Encapsulamento/Models/ContaConrrente.cs
@@ -0,0 +1,31 @@
+﻿namespace Encapsulamento.Models
+{
+    internal class ContaConrrente
+    {
+        public ContaConrrente(int numeroConta, decimal saldoInicial)
+        {
+            NumeroConta = numeroConta;
+            saldo = saldoInicial;
+        }
+        public int NumeroConta { get; set; }
+        private decimal saldo { get; set; }
+
+        public void Sacar(decimal valor)
+        {
+            if(saldo >= valor)
+            {
+                saldo -= valor;
+                Console.WriteLine("Saque realizado com sucesso!");
+            }
+            else
+            {
+                Console.WriteLine("Valor desejado é maior que o saldo disponível");
+            }
+        }
+
+        public void ExibirSaldo()
+        {
+            Console.WriteLine("Seu saldo disponível é:" + saldo);
+        }
+    }
+}
---
+++ Dev_Dotnet/Encapsulamento/Program.cs
@@ -0,0 +1,7 @@
+﻿using Encapsulamento.Models;
+
+ContaConrrente c1 = new(100, 1000);
+
+c1.ExibirSaldo();
+c1.Sacar(500);
+c1.ExibirSaldo();
