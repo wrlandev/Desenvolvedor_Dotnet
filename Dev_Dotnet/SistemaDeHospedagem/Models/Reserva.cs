2e2c4a799997c7b7ce3d7b93348ce5819341de38
Wrlan Silva <157828195+wrlandev@users.noreply.github.com>
2024-02-08 18:00:07 -0300

    Sistema de Hospedagem

---
+++ Dev_Dotnet/SistemaDeHospedagem/Models/Reserva.cs
@@ -0,0 +1,51 @@
+﻿namespace SistemaDeHospedagem.Models
+{
+    internal class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            if (Suite.Capacidade >= hospedes.Count)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                throw new Exception("A quantidade de hóspedes ultrupassa a capacidade da suite");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            if (DiasReservados >= 10)
+            {
+                var desconto = valor / 10;
+                valor = valor - desconto;
+            }
+
+            return valor;
+        }
+    }
+}
