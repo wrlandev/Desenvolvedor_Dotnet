6071ac5e67715ad1747ea865672f50cf952d909d
Wrlan Silva <157828195+wrlandev@users.noreply.github.com>
2024-02-08 20:31:13 -0300

    Abstração

---
+++ Dev_Dotnet/Abstracao/Models/Pessoa.cs
@@ -0,0 +1,13 @@
+﻿namespace Abstracao.Models
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public void Apresensar()
+        {
+            Console.WriteLine($"Ola meu nome é {Nome} e tenho {Idade} anos!");
+        }
+    }
+}
---
+++ Dev_Dotnet/Abstracao/Program.cs
@@ -0,0 +1,8 @@
+﻿using Abstracao.Models;
+
+Pessoa p1 = new();
+
+p1.Nome = ("Wrlan");
+p1.Idade = 20;
+
+p1.Apresensar();
